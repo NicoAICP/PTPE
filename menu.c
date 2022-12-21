33e74b4e6fbea031e8d2bca158e84eca40d36fa7
NicoAICP <nicohaberkorn@gmx.de>
2022-12-21 02:36:25 +0100

    Added Wifi Menu, TODO: need connecting to WiFi

--- menu.c
+++ menu.c
@@ -29,15 +29,32 @@ void drawError(int ErrCode){
         case 0: //No SD Card
             drawText(5, 5, "     Critical Error!     ", ST7735_RED, ST7735_BLACK, 1);
             drawFastHLine(0,20,200,ST7735_RED);
-            drawText(5, 37, "Unable to Mount SD Card", ST7735_RED, ST7735_BLACK, 1);
-            drawText(5, 49, "Possible Issues:", ST7735_WHITE, ST7735_BLACK, 1);
-            drawText(5, 61, "- Faulty / No SD Card", ST7735_WHITE, ST7735_BLACK, 1);
-            drawText(5, 73, "- Faulty Wiring", ST7735_WHITE, ST7735_BLACK, 1);
-            drawText(5, 85, "- Faulty SD Formating", ST7735_WHITE, ST7735_BLACK, 1);
+            drawText(5, 25, "Unable to Mount SD Card", ST7735_RED, ST7735_BLACK, 1);
+            drawText(5, 37, "Possible Issues:", ST7735_WHITE, ST7735_BLACK, 1);
+            drawText(5, 49, "- Faulty / No SD Card", ST7735_WHITE, ST7735_BLACK, 1);
+            drawText(5, 61, "- Faulty Wiring", ST7735_WHITE, ST7735_BLACK, 1);
+            drawText(5, 73, "- Faulty SD Formating", ST7735_WHITE, ST7735_BLACK, 1);
+            drawText(5, 97, "Press SELECT to Reboot", ST7735_WHITE, ST7735_BLACK, 1);
             drawFastHLine(0,108,200,ST7735_RED);
             drawText(5, 113, "Git Wiki: Troubleshoot SD", ST7735_RED, ST7735_BLACK, 1);
             break;
     }
+}
+
+void drawSetupMenu(int createdConfig){
+    clearScreen();
+    drawText(5, 5, "       WiFi  Setup       ", ST7735_WHITE, ST7735_BLACK, 1);
+    drawFastHLine(0,20,200,ST7735_WHITE);
+    drawText(5, 25, "Insert the SD into your PC", ST7735_WHITE, ST7735_BLACK, 1);
+    drawText(5, 37, "Edit config.ptpe:", ST7735_WHITE, ST7735_BLACK, 1);
+    drawText(5, 49, "- Fill WIFI_SSID", ST7735_WHITE, ST7735_BLACK, 1);
+    drawText(5, 61, "- Fill WIFI_PASS", ST7735_WHITE, ST7735_BLACK, 1);
+    drawText(5, 73, "Insert the SD into the Pi", ST7735_WHITE, ST7735_BLACK, 1);
+    if(createdConfig == 1){
+         drawText(5, 85, "Created config.ptpe on SD", ST7735_WHITE, ST7735_BLACK, 1);
+    }  
+    drawFastHLine(0,108,200,ST7735_WHITE);
+    drawText(5, 113, "- Press SELECT to Reboot ", ST7735_WHITE, ST7735_BLACK, 1);
 }
 
 void drawMenu(int sel){
@@ -60,7 +77,6 @@ void drawMenu(int sel){
 }
 
 void drawInitMenu(int sel){
-
     switch(sel){
         case 0:
             DeviceNum = 0;
@@ -99,6 +115,38 @@ void drawInitMenu(int sel){
     }
     
 }
+
+void drawWifiMenu(int sel){
+
+    switch(sel){
+        case 0:
+            drawText(5, 37, "-  Setup WiFi           <", ST7735_WHITE, ST7735_BLACK, 1);
+            drawText(5, 49, "-  Connect To WiFi       ", ST7735_WHITE, ST7735_BLACK, 1);
+            drawText(5, 61, "-  Start Transfer        ", ST7735_WHITE, ST7735_BLACK, 1);
+            drawText(5, 73, "-  Return                ", ST7735_WHITE, ST7735_BLACK, 1);
+            break;
+        case 1:
+            drawText(5, 37, "-  Setup WiFi            ", ST7735_WHITE, ST7735_BLACK, 1);
+            drawText(5, 49, "-  Connect To WiFi      <", ST7735_WHITE, ST7735_BLACK, 1);
+            drawText(5, 61, "-  Start Transfer        ", ST7735_WHITE, ST7735_BLACK, 1);
+            drawText(5, 73, "-  Return                ", ST7735_WHITE, ST7735_BLACK, 1);
+            break;
+        case 2:
+            drawText(5, 37, "-  Setup WiFi            ", ST7735_WHITE, ST7735_BLACK, 1);
+            drawText(5, 49, "-  Connect To WiFi       ", ST7735_WHITE, ST7735_BLACK, 1);
+            drawText(5, 61, "-  Start Transfer       <", ST7735_WHITE, ST7735_BLACK, 1);
+            drawText(5, 73, "-  Return                ", ST7735_WHITE, ST7735_BLACK, 1);
+            break;
+        case 3:
+            drawText(5, 37, "-  Setup WiFi            ", ST7735_WHITE, ST7735_BLACK, 1);
+            drawText(5, 49, "-  Connect To WiFi       ", ST7735_WHITE, ST7735_BLACK, 1);
+            drawText(5, 61, "-  Start Transfer        ", ST7735_WHITE, ST7735_BLACK, 1);
+            drawText(5, 73, "-  Return               <", ST7735_WHITE, ST7735_BLACK, 1);
+            break;
+    }
+    
+}
+
 
 void drawPressed(int i){
     char str[5];
